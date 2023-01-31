5244c160ed38a7f7b891f7ef91fad9f1b9bdc75e
ianstrom <iankstrom@gmail.com>
2023-01-30 22:06:07 -0500

    Finishing course work

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(symbol = '*') {
+    function flair(word = 'special') {
+        return `You are ${symbol}${word}${symbol}!`
+    }
+return flair;
+}
+
